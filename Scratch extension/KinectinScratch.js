9ecbfb83989f5f3c60a93e140442e37c2a896ee2
Isaac Zylstra <izylstra@gmail.com>
2015-07-28 15:13:45 -0400

    Adjust scratch alert text and fix #10

--- Scratch extension/KinectinScratch.js
+++ Scratch extension/KinectinScratch.js
@@ -8,7 +8,7 @@
     var zScale = 200;
     var status = 0;
     
-    alert("BEFORE CLICKING OK: Make sure you have have followed the instructions in Kinect2Scratch");
+    alert("BEFORE CLICKING OK: Make sure the kinect is on and KinectinScratchServer has started");
     
     var wsImpl = window.WebSocket || window.MozWebSocket;
      
@@ -103,12 +103,58 @@
         window.ws.close();
         console.log("connecting to local server ..");
         window.ws = new wsImpl('ws://localhost:8181/');
+        
+        // when data is comming from the server, this method is called
+    ws.onmessage = function (evt) {
+        jsonObject = JSON.parse(evt.data);
+            if(jsonObject.bodies == '')
+            {
+                status = 1;
+            } else
+            {
+                status = 2;
+            }
+    };
+
+    // when the connection is established, this method is called
+    ws.onopen = function () {
+        console.log('.. connection open');
+    };
+
+    // when the connection is closed, this method is called
+    ws.onclose = function () {
+        console.log('.. connection closed');
+        status = 0;
+    };
     };
     
     ext.ipconnect = function(s) {
         window.ws.close();
         console.log("connecting to "+s+' ..');
         window.ws = new wsImpl('ws://'+s+':8181/');
+        
+        // when data is comming from the server, this method is called
+    ws.onmessage = function (evt) {
+        jsonObject = JSON.parse(evt.data);
+            if(jsonObject.bodies == '')
+            {
+                status = 1;
+            } else
+            {
+                status = 2;
+            }
+    };
+
+    // when the connection is established, this method is called
+    ws.onopen = function () {
+        console.log('.. connection open');
+    };
+
+    // when the connection is closed, this method is called
+    ws.onclose = function () {
+        console.log('.. connection closed');
+        status = 0;
+    };
     }
     
     ext.closeconn = function() {
